e23ceeed6a3a6037ede9f598f202eeb862a6ab6c
Albert Montes <amontescobo@gmail.com>
2019-06-20 09:21:05 +0200

    Implement function and UT for checking model format.

--- graph_robot_mbt/graph_handler/path_generator.py
+++ graph_robot_mbt/graph_handler/path_generator.py
@@ -4,20 +4,28 @@ import subprocess
 
 class PathGenerator(object):
     def __init__(self):
-        self._logging = logging.getLogger(__name__)
+        self._logger = logging.getLogger(__name__)
 
     def check_model_format(self, model):
         """
         Checks that graphml model has a correct format
-        
+
         Args:
-            model(str): path to the .graphml model file 
+            model(str): path to the .graphml model file
 
         Returns:
             True if format is correct, False otherwise
         """
 
-        check_cmd = r'gw check -m {model_path} "random(edge_coverage(100))"'.format(model_path=correct_model))
-        response=subprocess.call(check_cmd)
+        check_cmd = (
+            r'gw check -m {model_path} "random(edge_coverage(100))"'
+            ).format(model_path=model)
+        response = subprocess.check_output(check_cmd)
+        
+        if 'No issues found with the model' in response:
+            return True
+        
+        else:
+            return False
 
     
--- graph_robot_mbt/graph_handler/tests/path_generator_tests.py
+++ graph_robot_mbt/graph_handler/tests/path_generator_tests.py
@@ -14,16 +14,23 @@ class PathGeneratorTests(unittest.TestCase):
         logging.basicConfig(level=logging.DEBUG)
         self._path_gen = PathGenerator()
 
-    @mock.patch('subprocess.call')
-    def test_check_model_format(self, call_mock):
+    @mock.patch('subprocess.check_output')
+    def test_check_model_format(self, check_out_mock):
         correct_model = 'correct_model.graphml'
-        call_mock.(
-            r'gw check -m {model_path} "random(edge_coverage(100))"'.format(
-                model_path=correct_model))
-        response = self._path_gen.check_model_format('dummy_model.graphml')
+        incorrect_model = 'incorrect_model.graphml'
+        check_out_mock.side_effect = ['No issues found with the model(s).',
+                                      'Error']
+        response = self._path_gen.check_model_format(correct_model)
         self.assertEqual(True, response)
-        call_mock.assert_called_once_with(
-            r'gw check -m {model_path} "random(edge_coverage(100))"'.format(
-                model_path=correct_model))
-
-        
+        response = self._path_gen.check_model_format(incorrect_model)
+        self.assertEqual(False, response)
+        check_out_mock.assert_has_calls(
+            [mock.call(
+                (
+                    r'gw check -m {model_path} "random(edge_coverage(100))"'
+                ).format(model_path=correct_model)),
+             mock.call(
+                (
+                    r'gw check -m {model_path} "random(edge_coverage(100))"'
+                ).format(model_path=incorrect_model))]
+        )
