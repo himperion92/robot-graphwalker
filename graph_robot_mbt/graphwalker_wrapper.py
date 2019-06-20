b2b5985f5e86ad38e509434a45be25bca7672523
Albert Montes <amontescobo@gmail.com>
2019-06-20 22:01:15 +0200

    Update project structure and implement sequence generator wrapper

---
+++ graph_robot_mbt/graphwalker_wrapper.py
@@ -0,0 +1,113 @@
+import logging
+import os
+import json
+import subprocess
+
+
+class GraphwalkerWrapper(object):
+    def __init__(self, graphwalker_location):
+        """
+        Initializes python graphwalker wrapper.
+
+        Args:
+            graphwalker_location (str): path to the location of graphwalker.jar
+                file.
+
+        Returns:
+            None.
+        """
+
+        self._logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.DEBUG)
+        self._cmd = r'java -jar {gw} "$@"'.format(gw=graphwalker_location)
+        self._sequence = []
+        self.a = ""
+
+    def check_model_format(self, model):
+        """
+        Checks that graphml model has a correct format
+
+        Args:
+            model (str): path to the .graphml model file.
+
+        Returns:
+            True if format is correct, False otherwise.
+        """
+
+        self._logger.info(
+            r'Checking "{model}" model format...'.format(model=model))
+        check_cmd = (
+            r'{cmd} check -m {model} "random(edge_coverage(100))"'
+            ).format(cmd=self._cmd, model=model)
+        response = subprocess.check_output(check_cmd, shell=True)
+
+        if 'No issues found with the model' in response:
+            self._logger.info('Model format is correct!')
+            return True
+
+        else:
+            self._logger.error('Model format is incorrect!')
+            return False
+
+    def generate_path(self, model, generator, stop_condition, condition):
+        """
+        Generates a execution path sequence following the selected strategy for
+        the given model.
+
+        Args:
+            model (str): path to the .graphml model file.
+            generator (str): possible values: random, weighted random,
+                quick_random, a_star.
+            stop_condition (str): possible values: edge_coverage,
+                vertex_coverage, requirement_coverage,
+                dependency_edge_coverage, reached_vertex, reached_edge,
+                time_duration, lenght, never.
+            condition: (str): possible values depend on stop_condition.
+                Normally, a number representing a given percentage or threshold
+                is required.
+
+        Returns:
+            None.
+        """
+
+        self._logger.info(
+            (
+                r'Generating execution path for "{model}" model...'
+            ).format(model=model))
+        offline_cmd = (
+            r'{cmd} offline -o -m {model_path} "random(edge_coverage(100))"'
+            ).format(cmd=self._cmd, model_path=model)
+        response = subprocess.check_output(offline_cmd, shell=True)
+        self._sequence = self._parse_sequence(response)
+        self._logger.info('Execution path successfully generated!')
+
+    def _parse_sequence(self, response):
+        """
+        Parses the output of graphwalker 'offline' command and translates it
+        into a dictionary.
+
+        Args:
+            response (str): output of graphwalker offline command.
+
+        Returns:
+            (list): sequence with complete information about nodes and edges.
+        """
+
+        sequence = []
+        self._logger.info('Parsing generated sequence...')
+        response = response.split(os.linesep)
+        if response[-1] == "":
+            del response[-1]
+
+        for step in response:
+            self._logger.debug(r'Step: "{step}"'.format(step=step))
+            print step
+            sequence.append(json.loads(step))
+
+        self._logger.info('Sequence successfully parsed!')
+        return sequence
+
+# if __name__ == '__main__':
+#     gw = GraphwalkerWrapper(r'/home/albert/workspace/projects/graphwalker-cli-3.4.2.jar')
+#     gw.check_model_format(r'/home/albert/workspace/projects/robot-graphwalker/demo/air_handler_system.graphml')
+#     gw.generate_path(r'/home/albert/workspace/projects/robot-graphwalker/demo/air_handler_system.graphml', 'random', 'edge_coverage', '100')
---
+++ graph_robot_mbt/tests/graphwalker_wrapper_tests.py
@@ -0,0 +1,76 @@
+import unittest
+import logging
+import os
+import mock
+
+from graph_robot_mbt.graphwalker_wrapper import GraphwalkerWrapper
+
+
+class GraphwalkerWrapperTests(unittest.TestCase):
+    """
+    Unitary Tests for GraphwalkerWrapper class.
+    """
+
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
+        self._gw = GraphwalkerWrapper(r'/home/bob/graphwalker-cli-3.4.2.jar')
+
+    def test_init(self):
+        self.assertEqual('java -jar /home/bob/graphwalker-cli-3.4.2.jar "$@"',
+                         self._gw._cmd)
+
+    @mock.patch('subprocess.check_output')
+    def test_check_model_format(self, subprocess_mock):
+        correct_model = 'correct_model.graphml'
+        incorrect_model = 'incorrect_model.graphml'
+        subprocess_mock.side_effect = ['No issues found with the model(s).',
+                                       'Error']
+        response = self._gw.check_model_format(correct_model)
+        self.assertEqual(True, response)
+        response = self._gw.check_model_format(incorrect_model)
+        self.assertEqual(False, response)
+        subprocess_mock.assert_has_calls(
+            [mock.call(r'{cmd} check -m {model_path} '
+                       r'"random(edge_coverage(100))"'.format(
+                           cmd=self._gw._cmd, model_path=correct_model),
+                       shell=True),
+             mock.call(
+                    r'{cmd} check -m {model_path} "random(edge_coverage(100))"'
+                    r''.format(cmd=self._gw._cmd, model_path=incorrect_model),
+                    shell=True)
+             ]
+        )
+
+    @mock.patch(('graph_robot_mbt.graphwalker_wrapper.GraphwalkerWrapper.'
+                 '_parse_sequence'))
+    @mock.patch('subprocess.check_output')
+    def test_generate_path(self, subprocess_mock, parse_seq_mock):
+        model = 'MyModel.graphml'
+        retrieved_paths = ''.join((r'{"modelName":"air_handler_system"}', os.linesep)) + ''.join((r'{"modelName":"air_handler_system","data":[],"currentElementID":"n1","currentElementName":"Idle","properties":[{"x":380},{"y":225}]}', os.linesep)) + ''.join((r'{"modelName":"air_handler_system","data":[],"currentElementID":"e2","currentElementName":"TurnAcOn","properties":[]}', os.linesep)) + ''.join((r'{"modelName":"air_handler_system","data":[],"currentElementID":"n3","currentElementName":"CoolingDown","properties":[{"x":240},{"y":321}]}', os.linesep)) + ''.join((r'{"modelName":"air_handler_system","data":[],"currentElementID":"e9","currentElementName":"ReachTemp","properties":[]}', os.linesep)) + ''.join((r'{"modelName":"air_handler_system","data":[],"currentElementID":"n1","currentElementName":"Idle","properties":[{"x":380},{"y":225}]}', os.linesep))
+        expected_path = [{"modelName": "air_handler_system"},
+                         {"modelName": "air_handler_system", "data": [],
+                          "currentElementID":"n1", 
+                          "currentElementName":"Idle",
+                          "properties":[{"x": 380}, {"y": 225}]},
+                         {"modelName": "air_handler_system", "data": [],
+                          "currentElementID":"e2",
+                          "currentElementName":"TurnAcOn", "properties":[]},
+                         {"modelName": "air_handler_system", "data": [],
+                          "currentElementID":"n3",
+                          "currentElementName":"CoolingDown",
+                          "properties":[{"x": 240}, {"y": 321}]},
+                         {"modelName": "air_handler_system", "data": [],
+                          "currentElementID":"e9",
+                          "currentElementName":"ReachTemp", "properties":[]},
+                         {"modelName": "air_handler_system",
+                          "data": [], "currentElementID":"n1",
+                          "currentElementName":"Idle",
+                          "properties":[{"x": 380}, {"y": 225}]}
+                         ]
+        parse_seq_mock.side_effect = [expected_path]
+        subprocess_mock.side_effect = [retrieved_paths]
+        self._gw.generate_path(model, 'random', 'edge_coverage', '100')
+        subprocess_mock.assert_called_once_with(
+            r'{cmd} offline -o -m {model} "random(edge_coverage(100))"'
+            ''.format(cmd=self._gw._cmd, model=model), shell=True)
+        self.assertEqual(expected_path, self._gw._sequence)
