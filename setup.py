674eff9711c951ea3c62eee750773623922f7779
himperion92 <amontescobo@gmail.com>
2020-04-26 11:23:16 +0200

    solve issue in setup.py regarding module name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from version import version
 setup(
     name='robot-graphwalker',
     version=version,
-    packages=['robot_model_based', 'robot_graphic_sequence'],
+    packages=['robot_model_based', 'robot_graphic_seq'],
     license='GNU General Public License v3.0',
     long_description=open('README.md').read()
 )
